3dc20ca4f23a3682b79fd017978916492ece0018
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-05-04 13:33:21 +0300

    Functions to deleted

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
-
 import "./style.css";
 import { addItem } from "./modules/addItem.js";
 
-
 const listContainer = document.querySelector("#list-container");
-
-export const toDoObjects = [];
-
-
-toDoObjects.forEach((task) => {
-  listContainer.innerHTML += `
-  <li class="to-do-item">
-  <input class="to-do-input" type="checkbox">
-  <p>${task.content}</p>
-  <span class="material-symbols-outlined">
-  more_vert
-  </span>
-  </li>`;
-});
-
 const addNewForm = document.querySelector("#add-list-form");
 const addTaskInput = document.querySelector("#add-task");
 
+if (!JSON.parse(localStorage.getItem("todolist"))) {
+  localStorage.setItem("todolist", JSON.stringify([]));
+}
 
-  addNewForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    toDoObjects.push(new addItem(addTaskInput.value));
+function display() {
+  let restoredTasks = JSON.parse(localStorage.getItem("todolist"));
+  restoredTasks.forEach((task) => {
+    listContainer.innerHTML += `
+    <li class="to-do-item">
+    <input class="to-do-input" type="checkbox">
+    <p>${task.content}</p>
+    <span class="material-symbols-outlined">
+    more_vert
+    </span>
+    </li>`;
   });
+}
 
-
+addNewForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let restoredItems = JSON.parse(localStorage.getItem("todolist"))
+  restoredItems.push(new addItem(addTaskInput.value));
+  localStorage.setItem("todolist", JSON.stringify(restoredItems));
+  display();
+});
