bfd66fe850412be578601dec6fce2f3e943e08c1
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-05-04 16:33:52 +0300

    Show last added task on the page

--- src/index.js
+++ src/index.js
@@ -2,31 +2,30 @@ import "./style.css";
 import { addItem } from "./modules/addItem.js";
 
 const listContainer = document.querySelector("#list-container");
+
+export const toDoObjects = [];
+
+function displayLastAddedElement() {
+  let lastEl = toDoObjects.slice(-1);
+  console.log(lastEl);
+console.log(toDoObjects);
+
+    listContainer.innerHTML += `
+  <li class="to-do-item">
+  <input class="to-do-input" type="checkbox">
+  <p>${lastEl[0].content}</p>
+  <span class="material-symbols-outlined">
+  more_vert
+  </span>
+  </li>`;
+  
+}
+
 const addNewForm = document.querySelector("#add-list-form");
 const addTaskInput = document.querySelector("#add-task");
 
-if (!JSON.parse(localStorage.getItem("todolist"))) {
-  localStorage.setItem("todolist", JSON.stringify([]));
-}
-
-function display() {
-  let restoredTasks = JSON.parse(localStorage.getItem("todolist"));
-  restoredTasks.forEach((task) => {
-    listContainer.innerHTML += `
-    <li class="to-do-item">
-    <input class="to-do-input" type="checkbox">
-    <p>${task.content}</p>
-    <span class="material-symbols-outlined">
-    more_vert
-    </span>
-    </li>`;
-  });
-}
-
 addNewForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  let restoredItems = JSON.parse(localStorage.getItem("todolist"))
-  restoredItems.push(new addItem(addTaskInput.value));
-  localStorage.setItem("todolist", JSON.stringify(restoredItems));
-  display();
+  toDoObjects.push(new addItem(addTaskInput.value));
+  displayLastAddedElement();
 });
