a154604a7202f856bd685c7b77cdf3b5a8423175
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-05-05 15:02:32 +0300

    Add yellow bg and trash icon

--- src/modules/edit.js
+++ src/modules/edit.js
@@ -1,17 +1,33 @@
+import display from "./display.js";
+
 export default function edit() {
-  const editForm = document.querySelectorAll('.editable-input');
+  const editForm = document.querySelectorAll(".editable-input");
 
   editForm.forEach((e) => {
-    const restoredData = JSON.parse(localStorage.getItem('todolist'));
-    e.addEventListener('click', (e)=>{
-      console.log(e.target);
-    })
-    e.addEventListener('keypress', (e) => {
-      if (e.key === 'Enter') {
-        e.target.blur();
-        restoredData[e.target.getAttribute('data-index')-1].content = e.target.value;
-      }
-      localStorage.setItem('todolist', JSON.stringify(restoredData));
+    const restoredData = JSON.parse(localStorage.getItem("todolist"));
+    e.addEventListener("click", (e) => {
+      let xselector = e.target;
+      let nextItem = xselector.nextElementSibling;
+      let parentEl = xselector.parentElement;
+      parentEl.style.backgroundColor = "yellow";
+
+      nextItem.innerHTML = "delete_forever";
+      keypressEnter();
     });
+    function keypressEnter() {
+      e.addEventListener("focusout", (e) => {
+        let targetEL = e.target;
+        let siblingSpan = targetEL.nextElementSibling;
+        let parentEl = targetEL.parentElement;
+        parentEl.style.backgroundColor = "transparent";
+        siblingSpan.innerHTML = "more_vert";
+        targetEL.blur();
+        restoredData[e.target.getAttribute("data-index") - 1].content =
+          e.target.value;
+
+        localStorage.setItem("todolist", JSON.stringify(restoredData));
+        display();
+      });
+    }
   });
 }
