b0d0c0fe6e1df320bd2c1431c1bf2e1320b69480
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-05-06 09:28:54 +0300

    Fix focusout function

--- src/modules/edit.js
+++ src/modules/edit.js
@@ -1,38 +1,41 @@
-import display from "./display.js";
-import indexSetter from "./indexSetter.js";
+import display from './display.js';
+import indexSetter from './indexSetter.js';
 
 export default function edit() {
-  const editForm = document.querySelectorAll(".editable-input");
+  const editForm = document.querySelectorAll('.editable-input');
 
   editForm.forEach((e) => {
-    e.addEventListener("click", (e) => {
+    e.addEventListener('click', (e) => {
       const xselector = e.target;
       const parentEl = xselector.parentElement;
-      parentEl.style.backgroundColor = "yellow";
+      parentEl.style.backgroundColor = 'yellow';
+
     });
   });
-  editForm.forEach((e) => {
-  e.addEventListener("focusout", (el) => {
-    const restoredData = JSON.parse(localStorage.getItem("todolist"));
-    const xselector = el.target;
-    const parentEl = xselector.parentElement;
-    parentEl.style.backgroundColor = "transparent";
-    parentEl.blur();
-    restoredData[el.target.getAttribute("data-index") - 1].content =
-      el.target.value;
 
-    localStorage.setItem("todolist", JSON.stringify(restoredData));
-    display();
-  });
-});
+  editForm.forEach((el) => {
+      el.addEventListener('focusout', (el) => {
+        const restoredData = JSON.parse(localStorage.getItem('todolist'));
+        const xselector = el.target;
+        const parentEl = xselector.parentElement;
+        parentEl.style.backgroundColor = 'transparent';
+        parentEl.blur();
+        restoredData[el.target.getAttribute('data-index') - 1].content = el.target.value;
 
-  const deleteButtons = document.querySelectorAll(".delete-icon");
+        localStorage.setItem('todolist', JSON.stringify(restoredData));
+        display();
+      });
+    })
+    
+
+
+  const deleteButtons = document.querySelectorAll('.delete-icon');
   deleteButtons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      const restoredData = JSON.parse(localStorage.getItem("todolist"));
-      const targetInd = e.target.getAttribute("data-index") - 1;
+    button.addEventListener('click', (e) => {
+      const restoredData = JSON.parse(localStorage.getItem('todolist'));
+      const targetInd = e.target.getAttribute('data-index') - 1;
       restoredData.splice(targetInd, 1);
-      localStorage.setItem("todolist", JSON.stringify(restoredData));
+      localStorage.setItem('todolist', JSON.stringify(restoredData));
       display();
       indexSetter();
     });
