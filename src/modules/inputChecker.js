a766caedd51a74a36696bdd0743095889a42ea3c
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-05-06 13:48:39 +0300

    Fix linters

--- src/modules/inputChecker.js
+++ src/modules/inputChecker.js
@@ -1,6 +1,6 @@
 export default function inputBoxChecker() {
   const toDoInputBox = document.querySelectorAll('.to-do-input');
-  
+
   toDoInputBox.forEach((element) => {
     element.addEventListener('click', (e) => {
       const restoredData = JSON.parse(localStorage.getItem('todolist'));
